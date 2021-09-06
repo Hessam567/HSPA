04fc901a2e5c7b221bb7547c656c6b2c8cdbd89d
myvsc21@outlook.com <h@sh.com>
2021-09-06 09:26:36 +0430

    Project init

---
+++ Frontend/src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,20 @@
+import { Component } from "@angular/core";
+
+// @Component({
+//   selector:'app-property-card',
+//   template:`<h1>test form PropertyCardComponent</h1>`,
+//   styles:['h1 {font-weight: normal;}']
+// })
+@Component({
+  selector:'app-property-card',
+  templateUrl:'property-card.component.html',
+  styleUrls:['property-card.component.css']
+})
+export class PropertyCardComponent {
+  Property:any={
+    "Id":1,
+    "Name":"Test1",
+    "Type":"House",
+    "Price":12000
+  }
+};
